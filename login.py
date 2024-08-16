651a1baa396abd9da375b5034927701181a37fde
ShivamD1310 <167774471+ShivamD1310@users.noreply.github.com>
2024-08-16 18:08:50 +0530

    Update login.py

--- login.py
+++ login.py
@@ -26,8 +26,7 @@ def scrape_profit_loss(cookies):
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         
-        table = soup.select_one('body main section:nth-of-type(5) div:nth-of-type(3) table')
-        headers = [th.text.strip() for th in table.select('thead tr th')]
+        headers = [th.text.strip() for th in soup.select('table.profit-loss thead tr th')]
         print("Headers:", '\t'.join(headers))
         
         rows = soup.select_one('body main section:nth-of-type(5) div:nth-of-type(3)').find_all('tr')
